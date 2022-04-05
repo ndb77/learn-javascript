9d0e8281cd953b1b6c2a14300dd5e7ebd104cfd0
ndb44 <kickyou77@gmail.com>
2022-04-05 14:17:42 -0400

    completed practice time calculator

--- 2. Practice time - part 1/6. Calculator challenge/index.js
+++ 2. Practice time - part 1/6. Calculator challenge/index.js
@@ -1,7 +1,10 @@
 let num1 = 8
 let num2 = 2
+let sum = 0
 document.getElementById("num1-el").textContent = num1
 document.getElementById("num2-el").textContent = num2
+sum_el = document.getElementById("sum-el")
+
 
 // Create four functions: add(), subtract(), divide(), multiply()
 // Call the correct function when the user clicks on one of the buttons
@@ -11,4 +14,19 @@ document.getElementById("num2-el").textContent = num2
 // E.g. if the user clicks on the "Plus" button, you should render
 // "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
 
-
+function add(){
+    sum = num1+num2
+    sum_el.textContent = "Sum: " + String(sum)
+}
+function subtract(){
+    sum = num1-num2
+    sum_el.textContent = "Sum: " + String(sum)
+}
+function multiply(){
+    sum = num1*num2
+    sum_el.textContent = "Product: " + String(sum)
+}
+function divide(){
+    sum = num1/num2
+    sum_el.textContent = "Dividend: " + String(sum)
+}
---
+++ 2. Practice time - part 1/6. Calculator challenge/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+let num1 = 8
+let num2 = 2
+let sum = 0
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+document.getElementById("sum-el").textContent = sum_el
